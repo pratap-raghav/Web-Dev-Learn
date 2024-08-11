10c67bed06844670a299b81bf41ed5e4624ae49c
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-11 16:47:37 +0530

    ...

--- Lesson33/script.js
+++ Lesson33/script.js
@@ -1,3 +1,7 @@
 console.log("Console Intialized")
 
 var container = document.body.childNodes[1]
+console.log("Title- ",document.title)
+container.style.backgroundColor ="wheat"
+console.log(document.body)
+console.log(container.children)
