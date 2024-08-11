29d1cbb21b039c62f279ad594007336a6bf546be
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-11 17:11:39 +0530

    Update script.js

--- Lesson33/script.js
+++ Lesson33/script.js
@@ -5,3 +5,12 @@ console.log("Title- ",document.title)
 container.style.backgroundColor ="wheat"
 console.log(document.body)
 console.log(container.children)
+var box = container.children
+console.log(box)
+console.log("Length(box)- ",box.length, typeof box)
+
+for (let i = 0; i < box.length; i++) {
+    const element = box[i];
+    console.log(i+"-", element)
+    
+}
