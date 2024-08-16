e7f931f5a4e3af63bc36bd59c90bddcd8e47be47
pratap-raghav <pratap.raghav.dev@gmail.com>
2024-08-16 21:21:36 +0530

    ...

--- Exercise9/script.js
+++ Exercise9/script.js
@@ -1,23 +1,14 @@
 var container = document.body.children[0].children[0].children
-console.log(container)
 
-colorObj = {
-    1:"red",
-    2:"blue",
-    3:"white",
-    4:"orange",
-    5:"pink",
-    6:"aqua",
-    7:"yellow",
-    8:"green",
-    9:"brown",
-    0:"violet"
-}
 for (let index = 0; index < container.length; index++) {
     const element = container[index];
-    var value = Math.floor((Math.random())*10)
-    console.log(value)
-    element.style.backgroundColor = colorObj[value]
-    console.log(element)
+    
+    var r = Math.floor(Math.random()*255)
+    var b = Math.floor(Math.random()*255)
+    var g = Math.floor(Math.random()*255)
+
+
+    console.log(r,g,b)
+    element.style.backgroundColor = `rgb(${r},${g},${b})`
     
 }
